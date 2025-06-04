513e1fb9ddcc7467caad5dc3db0b991070b98e51
CISSE <152055855+cissealseny@users.noreply.github.com>
2025-06-04 15:13:50 +0100

    Update 0001_initial.py

--- django_cron/migrations/0001_initial.py
+++ django_cron/migrations/0001_initial.py
@@ -1,47 +1,23 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-
 class Migration(migrations.Migration):
 
+    initial = True
     dependencies = []
 
     operations = [
         migrations.CreateModel(
             name='CronJobLog',
             fields=[
-                (
-                    'id',
-                    models.AutoField(
-                        verbose_name='ID',
-                        serialize=False,
-                        auto_created=True,
-                        primary_key=True,
-                    ),
-                ),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
                 ('code', models.CharField(max_length=64, db_index=True)),
                 ('start_time', models.DateTimeField(db_index=True)),
                 ('end_time', models.DateTimeField(db_index=True)),
                 ('is_success', models.BooleanField(default=False)),
                 ('message', models.TextField(max_length=1000, blank=True)),
-                (
-                    'ran_at_time',
-                    models.TimeField(
-                        db_index=True, null=True, editable=False, blank=True
-                    ),
-                ),
+                ('ran_at_time', models.TimeField(null=True, blank=True, db_index=True, editable=False)),
             ],
-        ),
-        migrations.AlterIndexTogether(
-            name='cronjoblog',
-            index_together=set(
-                [
-                    ('code', 'is_success', 'ran_at_time'),
-                    ('code', 'start_time', 'ran_at_time'),
-                    ('code', 'start_time'),
-                ]
-            ),
         ),
     ]
